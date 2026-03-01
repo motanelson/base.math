e4ece1996f175a6a14ec8b71d94b9fd7eb70986b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-03-01 12:19:23 +0000

    Add files via upload

---
+++ max.py
@@ -0,0 +1,23 @@
+print("\033c\033[40;37m\n")
+import copy
+def getlist(i:int): 
+    l:list=[]
+    for a in range(i):
+        l.append(copy.copy(a))
+    return l
+
+def maxs(l:list):
+    a:int=0
+    count=0
+    for aa in l:
+        if count==0:
+            a=aa
+        else:
+            if aa>a:
+                a=aa
+    return a
+
+
+l=getlist(20)
+print(l)
+print(max(l))
